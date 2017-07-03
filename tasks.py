87bf75bceac78fafc90d82a9aa3b6315a2982868
vi01et03 <kairos9603@gmail.com>
2017-07-03 20:35:26 +0900

    test bitcoin

--- tasks.py
+++ tasks.py
@@ -1,6 +1,20 @@
 from microsoftbotframework import ReplyToActivity
+import requests
 
 def echo_response(message):
   if message["type"] == "message":
     ReplyToActivity(fill=message,
                     text=message["text"]).send()
+
+
+def cost_search(message):
+  res = requests.get("https://api.korbit.co.kr/v1/ticker")
+  res.json()
+
+  last = res['last']
+
+  msg = "last cost : {}".format(last)
+
+  if message["type"] == "message" and message["text"] == "bitcoin":
+    ReplyToActivity(fill=message,
+                    text=msg).send()
